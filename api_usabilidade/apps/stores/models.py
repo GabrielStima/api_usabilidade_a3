e24b468c9af5adf1cd11f69a0fb4a5ed21cb99b9
Vitor Antoni <v.silva.antoni@gmail.com>
2023-12-03 02:43:02 -0300

    App stores update

--- api_usabilidade/apps/stores/models.py
+++ api_usabilidade/apps/stores/models.py
@@ -5,10 +5,10 @@ import uuid
 # Create your models here.
 class Store(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    store_name = models.CharField(max_length=20, blank=False)
+    name = models.CharField(max_length=20, blank=False)
     cnpj = models.IntegerField(unique=True, blank=False)
     address = models.CharField(max_length=50, blank=False)
-    store_owner = models.ForeignKey(Customer, on_delete=models.CASCADE)     # Quando o customer for deletado, STORE também será
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)     # Quando o customer for deletado, STORE também será
 
 
     class Meta:
--- api_usabilidade/apps/stores/views.py
+++ api_usabilidade/apps/stores/views.py
@@ -1,29 +1,26 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, status
 from django.shortcuts import get_object_or_404
 from . import models
 from . import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 
 
 # Create your views here.
 class StoreViewset(APIView):
+    queryset = models.Store.objects.all()
+
     def get(self, request, id=None):
         if id:
-            item = models. Store.objects.get(id=id)
-            serializer = serializers.StoreSerializer (item)
+            item = models.Store.objects.get(id=id)
+            serializer = serializers.StoreSerializer(item)
 
-            return Response({
-                "status": "success",
-                "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response({serializer.data}, status=status.HTTP_200_OK)
         
         items = models.Store.objects.all()
         serializer = serializers.StoreSerializer(items, many=True)
 
-        return Response({
-            "status:":"success",
-            "data":serializer.data}, status=status.HTTP_200_OK)
+        return Response({serializer.data}, status=status.HTTP_200_OK)
     
     def post(self, request):
         serializer = serializers.StoreSerializer(data=request.data)
@@ -31,13 +28,9 @@ class StoreViewset(APIView):
         if serializer.is_valid():
             serializer.save()
 
-            return Response({
-                "status":"success",
-                "data":serializer.data}, status=status.HTTP_200_OK)
+            return Response({serializer.data}, status=status.HTTP_200_OK)
         else:
-            return Response({
-                "status":"error",
-                "data":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request, id=None):
         item = models.Store.objects.get(id=id)
@@ -46,20 +39,16 @@ class StoreViewset(APIView):
         if serializer.is_valid():
             serializer.save()
 
-            return Response({
-                "status":"success",
-                "data":serializer.data}, status=status.HTTP_200_OK)
+            return Response({serializer.data}, status=status.HTTP_200_OK)
         else:
-            return Response({
-                "status":"error",
-                "data":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
     def delete(self, request, id=None):
         item = models.Store.objects.filter(id=id)
         item.delete()
 
-        return Response({
-            "status":"success",
-            "data":"Item Deleted"
-        })
- 
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class StoreViewset_UUID(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Store.objects.all()
+    serializer_class = serializers.StoreSerializer
