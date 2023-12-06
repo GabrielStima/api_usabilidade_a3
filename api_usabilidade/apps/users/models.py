de190cef48c65c377a43e8c191c9dbb9bc199e72
Vitor Antoni <v.silva.antoni@gmail.com>
2023-12-06 17:33:03 -0300

    Update app users

--- api_usabilidade/apps/users/models.py
+++ api_usabilidade/apps/users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     firstname = models.CharField(max_length=30, blank=False)
     lastname = models.CharField(max_length=50, blank=False)
     email = models.EmailField(blank=False)
